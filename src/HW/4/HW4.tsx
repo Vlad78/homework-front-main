22d6d4aaa506df23ed55b15a416ffaae4e38677f
Vlad Razvenkov <dssdom@gmail.com>
2024-03-19 14:45:56 +0100

    fix: 4th homework

--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
-import s from './HW4.module.css';
-import { Button } from './Button';
-import { Input } from './Input';
+import { useState } from "react";
+
+import { Button } from "./Button";
+import s from "./HW4.module.css";
+import { Input } from "./Input";
 
 export const HW4 = () => {
   // 1️⃣ Раскомментируйте JSX (HW4.tsx) и вы увидите,
@@ -14,34 +15,38 @@ export const HW4 = () => {
 
   // ❗ Массив с данными не трогаем! Текст не меняем !
 
-  const [currentText, setCurrentText] = useState('');
+  const [currentText, setCurrentText] = useState("");
   const [texts, setTexts] = useState<string[]>([
-    'То, что вы делаете по ночам, то и делает вас богатым. (Аль Капоне)',
+    "То, что вы делаете по ночам, то и делает вас богатым. (Аль Капоне)",
   ]);
 
   const handleSave = () => {
     setTexts([currentText, ...texts]);
-    setCurrentText('');
+    setCurrentText("");
   };
 
   return (
-    <div id={'hw04'}>
+    <div id={"hw04"}>
       {currentText ? (
-        <h1 id={'hw04-text'}>{currentText}</h1>
+        <h1 id={"hw04-text"}>{currentText}</h1>
       ) : (
-        <h1 id={'hw04-default-text'}>Здесь появится новое дело</h1>
+        <h1 id={"hw04-default-text"}>Здесь появится новое дело</h1>
       )}
 
       <Input currentText={currentText} setCurrentText={setCurrentText} />
 
       <Button callBack={handleSave} name="Кнопка" />
 
-      <h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
+      <h1 style={{ marginTop: "50px" }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
 
-      <ol id={'hw04-tasks'}>
+      <ol id={"hw04-tasks"}>
         {texts.map((el, index) => {
           return (
-            <li key={index} id={`hw04-task-${index}`} className={index%2 === 1 ? s.chetNechet : ''}>
+            <li
+              key={index}
+              id={`hw04-task-${index}`}
+              className={index % 2 === 0 ? s.chetNechet : ""}
+            >
               {el}
             </li>
           );
@@ -50,4 +55,3 @@ export const HW4 = () => {
     </div>
   );
 };
-
